ced36b3eedd2dd57b09a0d4042c5bfdc4ff5194e
Elliot Saba <staticfloat@gmail.com>
2025-03-02 02:42:18 +0000

    LLVMCompilerRT v17.0.7

--- src/LLVMCompilerRT_jll.jl
+++ src/LLVMCompilerRT_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule LLVMCompilerRT_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module LLVMCompilerRT_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module LLVMCompilerRT_jll
 
-JLLWrappers.@generate_main_file_header("LLVMCompilerRT")
-JLLWrappers.@generate_main_file("LLVMCompilerRT", UUID("4e17d02c-6bf5-513e-be62-445f41c75a11"))
-end  # module LLVMCompilerRT_jll
